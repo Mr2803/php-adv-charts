a6a30fa2141fa65e599b151d0ca89975e294be0c
Mr2803 <rispoli.mar2803@gmail.com>
2019-12-13 16:52:18 +0100

    finish exercise , but have a bug

--- step3/getAllChart.php
+++ step3/getAllChart.php
@@ -1,4 +1,13 @@
 <?php 
 header('Content-Type: application/json');
 include "data3.php";
-echo json_encode($graphs);
+$access = $_GET['level'];
+$result = [$graphs['fatturato']];
+
+ if ($access == $graphs['fatturato_by_agent']['access']) {
+    $result[] = $graphs['fatturato_by_agent'];
+} else if ($access == $graphs['team_efficiency']['access']) {
+    $result[] = $graphs['fatturato_by_agent'];
+    $result[] = $graphs['team_efficiency'];
+}
+echo json_encode($result);
--- step3/script/script.js
+++ step3/script/script.js
@@ -1,148 +1,160 @@
 $(document).ready(init);
 
 function init() {
-    
-    getAjax();
+   
+   getAjax();
 }
 
 function printLineFatturato(data){
-    
-    var ctx = document.getElementById('myChartLine').getContext('2d');
-    new Chart(ctx, {
-        type: data["type"],
-        data: {
-            labels: moment.months(),
-            datasets: [{
-                label: 'Vendite',
-                data: data["data"],
-                backgroundColor:'rgba(255, 206, 86, 0.2)',
-                borderColor:'rgba(255, 159, 64, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            legend: {
-                labels: {
-                    fontColor: 'black',
-                    fontSize: 15,
-                    fontStyle: "bold"
-                }
-            },
+   
+   var ctx = document.getElementById('myChartLine').getContext('2d');
+   new Chart(ctx, {
+      type: data["type"],
+      data: {
+         labels: moment.months(),
+         datasets: [{
+               label: 'Vendite',
+               data: data["data"],
+               backgroundColor:'rgba(255, 206, 86, 0.2)',
+               borderColor:'rgba(255, 159, 64, 1)',
+               borderWidth: 1
+         }]
+      },
+      options: {
+         legend: {
+               labels: {
+                  fontColor: 'black',
+                  fontSize: 15,
+                  fontStyle: "bold"
+               }
+         },
 
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    })
+         scales: {
+               yAxes: [{
+                  ticks: {
+                     beginAtZero: true
+                  }
+               }]
+         }
+      }
+   })
 }
 
 function printPieFatturatoAgent(data){
-    var keys = Object.keys(data["data"]);
-    var values = Object.values(data["data"]);
-    var ctx = document.getElementById('myChartPie').getContext('2d');
-    new Chart(ctx, {
-        type: data["type"],
-        data: {
-            labels: keys,
-            datasets: [{
-                data: values,
-                backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                borderColor: 'rgba(255, 159, 64, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            legend: {
-                labels: {
-                    fontColor: 'black',
-                    fontSize: 15,
-                    fontStyle: "bold"
-                }
-            },
-
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    })
+   var keys = Object.keys(data["data"]);
+   var values = Object.values(data["data"]);
+   var ctx = document.getElementById('myChartPie').getContext('2d');
+   new Chart(ctx, {
+      type: data["type"],
+      data: {
+         labels: keys,
+         datasets: [{
+               data: values,
+               backgroundColor: 'rgba(255, 206, 86, 0.2)',
+               borderColor: 'rgba(255, 159, 64, 1)',
+               borderWidth: 1
+         }]
+      },
+      options: {
+         legend: {
+               labels: {
+                  fontColor: 'black',
+                  fontSize: 15,
+                  fontStyle: "bold"
+               }
+         },
+         scales: {
+               yAxes: [{
+                  ticks: {
+                     beginAtZero: true
+                  }
+               }]
+         }
+      }
+   })
 }
 
 function printEfficiency(data) {
-    var keys = Object.keys(data["data"]);
-    console.log(keys)
-    var values = Object.values(data["data"]);
-    var ctx = document.getElementById('myChartEfficiency').getContext('2d');
-    new Chart(ctx, {
-        type: data["type"],
-        data: {
-            labels: moment.months(),
-            datasets: [
-                {
-                label: keys[0],
-                data: values[0],
-                backgroundColor: 'rgba(255, 16, 86, 0.3)',
-                borderColor: 'rgba(255, 16, 64, 1)',
-                borderWidth: 1
-                },
-                {
-                label: keys[1],
-                data: values[1],
-                backgroundColor: 'rgba(138, 182, 255, 0.4)',
-                borderColor: 'rgba(138, 190, 255, 1)',
-                borderWidth: 1
-                },
-                {
-                label: keys[2],
-                data: values[2],
-                backgroundColor: 'rgba(43, 900, 126, 0.7)',
-                borderColor: 'rgba(43, 159, 64, 1)',
-                borderWidth: 1
-                }
-            ]
-        },
-        options: {
-            legend: {
-                labels: {
-                    fontColor: 'black',
-                    fontSize: 15,
-                    fontStyle: "bold"
-                }
-            },
+   var keys = Object.keys(data["data"]);
+   console.log(keys)
+   var values = Object.values(data["data"]);
+   var ctx = document.getElementById('myChartEfficiency').getContext('2d');
+   new Chart(ctx, {
+      type: data["type"],
+      data: {
+         labels: moment.months(),
+         datasets: [
+               {
+               label: keys[0],
+               data: values[0],
+               backgroundColor: 'rgba(255, 16, 86, 0.3)',
+               borderColor: 'rgba(255, 16, 64, 1)',
+               borderWidth: 1
+               },
+               {
+               label: keys[1],
+               data: values[1],
+               backgroundColor: 'rgba(138, 182, 255, 0.4)',
+               borderColor: 'rgba(138, 190, 255, 1)',
+               borderWidth: 1
+               },
+               {
+               label: keys[2],
+               data: values[2],
+               backgroundColor: 'rgba(43, 900, 126, 0.7)',
+               borderColor: 'rgba(43, 159, 64, 1)',
+               borderWidth: 1
+               }
+         ]
+      },
+      options: {
+         legend: {
+               labels: {
+                  fontColor: 'black',
+                  fontSize: 15,
+                  fontStyle: "bold"
+               }
+         },
 
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    })
+         scales: {
+               yAxes: [{
+                  ticks: {
+                     beginAtZero: true
+                  }
+               }]
+         }
+      }
+   })
 }
 
 function getAjax() {
+   var url = window.location.href;
+   console.log("Url:", url);
 
-    $.ajax({
+   var elem = url.split("=");
+   console.log("elemento:", elem)
 
-        url: "getAllChart.php",
-        method: "GET",
-        success: function (data) {
-            printLineFatturato(data["fatturato"]);
-            printPieFatturatoAgent(data["fatturato_by_agent"]);
-            printEfficiency(data["team_efficiency"])
-        },
-        error: function (err) {
-            console.log("error", err)
-        }
-    });
+   var liv = elem[1];
+   
+   console.log("questo è il log di liv " + liv);
+
+   $.ajax({
+
+      url: "getAllChart.php",
+      method: "GET",
+      data : {
+         level : liv
+      },
+      success: function (data) {
+         console.log(data)
+         printLineFatturato(data[0]);
+         printPieFatturatoAgent(data[1]);
+         printEfficiency(data[2]);
+      },
+      error: function (err) {
+         console.log("error", err)
+      }
+   });
 }
 
 
