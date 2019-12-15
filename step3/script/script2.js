360c9dd26330b7e197ba30debd0665a9776976d3
Mr2803 <rispoli.mar2803@gmail.com>
2019-12-15 15:25:12 +0100

    fix script2 in step3

--- step3/script/script2.js
+++ step3/script/script2.js
@@ -1,20 +1,16 @@
-$(document).ready(init);
-
-function init() {
-
-   getAjax();
+//funzione per farmi restituire l'oggetto datasets
+function datasets(label, data, bgC, bdC) {
+   var object = {
+      label: label,
+      data: data,
+      backgroundColor: bgC,
+      borderColor: bdC,
+      borderWidth: 1
+   }
+   return object
 }
 
-function getRandomColor() {
-   const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-   const randomByte = () => randomNumber(0, 255);
-   console.log(randomByte);
-   const randomPercent = () => (randomNumber(50, 100) * 0.01).toFixed(2);
-   const randomCssRgba = () => `rgba(${[randomByte(), randomByte(), randomByte(), randomPercent()].join(',')})`;
-
-   return randomCssRgba;
-}
-
+//funzione generica per stampa grafici
 function printChart(wherePrint, chartType, labels, label, data,bgColor,bordColor) {
    var ctx = $('#' + wherePrint);
    new Chart(ctx, {
@@ -27,37 +23,34 @@ function printChart(wherePrint, chartType, labels, label, data,bgColor,bordColor
       },
       options: {
          legend: {
-               labels: {
-                  fontColor: 'black',
-                  fontSize: 15,
-                  fontStyle: "bold"
-               }
+            labels: {
+               fontColor: 'black',
+               fontSize: 15,
+               fontStyle: "bold"
+            }
          },
-
          scales: {
-               yAxes: [{
-                  ticks: {
-                     beginAtZero: true
-                  }
-               }]
+            yAxes: [{
+               ticks: {
+                  beginAtZero: true
+               }
+            }]
          }
       }
    })
 }
 
-function datasets(label,data, bgC, bdC){
-   var object={
-         label: label,
-         data: data,
-         backgroundColor: bgC,
-         borderColor: bdC,
-         borderWidth: 1
-      }
-   return object
+function getRandomColor() {
+   const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+   const randomByte = () => randomNumber(0, 255);
+   console.log(randomByte);
+   const randomPercent = () => (randomNumber(50, 100) * 0.01).toFixed(2);
+   const randomCssRgba = () => `rgba(${[randomByte(), randomByte(), randomByte(), randomPercent()].join(',')})`;
+   return randomCssRgba;
 }
+
 function printLineFatturato(data) {
    printChart('myChartLine', data["type"], moment.months(), "Vendite", data["data"], 'rgba(255, 206, 86, 0.2)', 'rgba(255, 159, 64, 1)')
-
 }
 
 function printPieFatturatoAgent(data) {
@@ -65,7 +58,6 @@ function printPieFatturatoAgent(data) {
    var values = Object.values(data["data"]);
    printChart('myChartPie', data["type"], keys, "", values, getRandomColor(),"grey")
 }
-
 
 function printEfficiency(data) {
    var keys = Object.keys(data["data"]);
@@ -75,7 +67,6 @@ function printEfficiency(data) {
       type: data["type"],
       data: {
          labels: moment.months(),
-         
          datasets: [
             datasets(keys[0], values[0], 'rgba(255, 16, 86, 0.3)', 'rgba(255, 16, 64, 1)'),
             datasets(keys[1], values[1], 'rgba(138, 182, 255, 0.4)', 'rgba(138, 190, 255, 1)'),
@@ -90,7 +81,6 @@ function printEfficiency(data) {
                   fontStyle: "bold"
                }
          },
-
          scales: {
                yAxes: [{
                   ticks: {
@@ -102,32 +92,34 @@ function printEfficiency(data) {
    })
 }
 
-function getAjax() {
-
+function getLevel(){
    var url = window.location.href;
-   console.log("Url:", url);
    var elem = url.split("http://localhost/step3/?level=");
-   console.log("elemento:", elem);
    var liv = elem[1];
-   console.log("questo è il log di liv " + liv);
+   return liv;
+}
 
+function getAjax() {
    $.ajax({
       url: "getAllChart.php",
       method: "GET",
       data: {
-         level: liv
+         level: getLevel()
       },
       success: function (data) {
          console.log(data)
-
-
          printLineFatturato(data[0]);
          printPieFatturatoAgent(data[1]);
          printEfficiency(data[2]);
-
       },
       error: function (err) {
          console.log("error", err)
       }
    });
+}
+
+$(document).ready(init);
+
+function init() {
+   getAjax();
 }
